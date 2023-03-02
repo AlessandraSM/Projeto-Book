f0ff30b443be61da8635defb1b52862283c5e066
Alessandra-Marmiroli <alessandramarmiroli@gmail.com>
2023-03-02 15:40:29 -0300

    Fazendo Filter e Map em array de Livros

---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,9 @@
+import livros from '../../img/livro.png'
+
+export const livrosNovos = [
+    {nome: 'Liderança UX', id: 1, src:livros},
+    {nome: 'HTML', id: 2, src:livros},
+    {nome: 'CSS', id: 3, src:livros},
+    {nome: 'Tela UI', id: 4, src:livros},
+    {nome: 'Full Stack para todos', id: 5, src:livros},
+]
